54897814224923616b359183b4b53857a82909c7
esenecal <55216700+esenecal@users.noreply.github.com>
2026-06-06 16:31:31 -0700

    Added bfs_dist

--- bfs.py
+++ bfs.py
@@ -3,14 +3,14 @@
 
 from collections import deque       # for the use of queues. https://www.geeksforgeeks.org/python/queue-in-python/
 
-def bfs_goal(graph, root, goal):
+def bfs_goal(graph, root, goal):    # find shortest path to goal.
     # verify the root is in the graph.
     if root not in graph.keys():
         return 0
     
-    q = deque()         # initialize queue
-    visited = {root}    # add root to visited set
-    q.append(root)      # add root to queue
+    q = deque()                 # initialize queue
+    visited = {root}            # add root to visited set
+    q.append(root)              # add root to queue
     parent_nodes = {}           # create an empty dictionary with the keys of the graph.
     for key in graph.key():
         parent_nodes[key] = None
@@ -35,3 +35,30 @@ def bfs_goal(graph, root, goal):
                 visited.add(node)               # mark it as visited
                 parent_nodes[node] = vertex     # mark vertex as node's parent.
                 q.append(node)                  # add node to the queue so we can iterate through its children later
+
+# returns dictionary with distance from root to each node
+def bfs_dist(graph, root):
+    # verify root is in graph
+    if root not in graph.keys():
+        return 0
+    
+    q = deque()             # initialize queue
+    visited = {root}        # add root to visited set
+    q.append(root)          # add root to q
+    d = {}                  # create empty dictionary with keys for each node
+    for key in graph.keys():
+        d[key] = 0          # distance is, by default, 0.
+    
+    i = 1                   # iterable for tracking distance
+    while len(q) != 0:      # while our q is not empty:
+
+        vertex = q.popleft()        # get node on top of q, name it vertex.
+
+        # for each node connected to the vertex
+        for node in graph.get(vertex):
+            if node not in visited:     # if node has not been visited.
+                visited.add(node)       # add node to visited
+                d[node] = i             # ad distance from root to d
+                q.append(node)          # add node to q
+        i += 1
+    return d
