c36ad24466dfe717760c37e152fb109e7eb0044c
esenecal <55216700+esenecal@users.noreply.github.com>
2026-06-12 22:42:45 -0700

    Beginning a*

---
+++ a_star.py
@@ -0,0 +1,7 @@
+# A* algorithm. Any heuristic system works; try the general distance dictionary from the bfs as the heuristic algorithm 
+# https://en.wikipedia.org/wiki/A*_search_algorithm
+
+# graph: graph for traversal. root: starting node. goal: goal node. h: heuristic dictionary.
+def a_star(graph, root, goal, h):
+
+    return 0
