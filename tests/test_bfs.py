e33d3bb6d09dfcd29c13df9600f7d4a944879bc1
esenecal <55216700+esenecal@users.noreply.github.com>
2026-06-17 17:08:02 -0700

    Added test cases for new bfs function

--- tests/test_bfs.py
+++ tests/test_bfs.py
@@ -1,4 +1,4 @@
-from bfs import bfs_dist, bfs_goal
+from bfs import bfs_dist, bfs_goal, bfs_dist_weighted
 
 import pytest
 
@@ -15,7 +15,7 @@ test_weighted_graph_1 = {       # each has a tuple showing the weight between th
     "a": [("b", 2), ("d", 1)],
     "b": [("a", 2), ("c", 3), ("d", 2), ("e", 4), ("f", 5)], 
     "c": [("b", 3), ("e", 1)], 
-    "d": [("a", 1), ("b", 2), ("e", 4)], 
+    "d": [("a", 1), ("b", 2), ("e", 3)], 
     "e": [("b", 4), ("c", 1), ("d", 3), ("f", 2)], 
     "f": [("b", 5), ("e", 2)]
 }
@@ -109,3 +109,29 @@ def test_bfs_dist_ug1_no_root():
         'Error: z not in graph'
         in str(exception.value)
     )
+
+def test_bfs_dist_weighted_f():
+
+    h = bfs_dist_weighted(test_weighted_graph_1, "f")
+
+    assert h == {
+        "a": 2,
+        "b": 1,
+        "c": 2,
+        "d": 2,
+        "e": 1,
+        "f": 0
+    }
+
+def test_bfs_dist_weighted_a():
+
+    h = bfs_dist_weighted(test_weighted_graph_1, "a")
+
+    assert h == {
+        "a": 0,
+        "b": 1,
+        "c": 2,
+        "d": 1,
+        "e": 2,
+        "f": 2
+    }
