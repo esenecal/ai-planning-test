2b2363329e312ea3b5986979897dad9456651d97
esenecal <55216700+esenecal@users.noreply.github.com>
2026-07-09 16:44:38 -0700

    Edited the README and notes.

--- src/node.py
+++
@@ -1,7 +0,0 @@
-class Node:
-    """
-    A node has a name. Adjacent nodes will be tracked by a dictionary. The dictionary will be used because the graph will not
-    be changed during runtime.
-    """
-    def __init__(self, name: str) -> None:
-        self.name = name
