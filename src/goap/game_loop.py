9b9fdd8e5fdd6a9a0604ebbafc4785d633f0d42c
esenecal <55216700+esenecal@users.noreply.github.com>
2026-07-13 21:32:28 -0700

    Beginning work on the basic loop

--- src/goap/game_loop.py
+++ src/goap/game_loop.py
@@ -1,1 +1,22 @@
 # This handles the game loop, where the game is driven.
+
+# The loop is very simple. The user has a chance to input 
+
+exit = True
+
+# processes user input.
+def process_input(user_input):
+    global exit
+    if user_input == 'exit':
+        exit = False
+        return "Exiting"
+    else:
+        return 1
+
+# The loop
+while exit:
+    user_input = input()                # get user input
+    output = process_input(user_input)  # process it
+    print(output)                       # Display a message
+
+
