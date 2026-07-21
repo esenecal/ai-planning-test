2b21f5d7fbf2dd1ef7ae7191f89d8c61dd7e42fd
esenecal <55216700+esenecal@users.noreply.github.com>
2026-07-21 14:46:59 -0600

    Implemented part of the character class. Working on planner.

--- src/goap/character.py
+++ src/goap/character.py
@@ -5,5 +5,30 @@ class Character:
 
     def __init__(self, name: str, goals: list[tuple[int, Goal]] ) -> None:
         self.name = name
-        self.goals = goals                      # simply for reference.
-        self.goal_queue = heapq.heapify(goals)  # convert goals into a priority queue.
+        self.goals = goals                      # simply for reference. helpful if we want the original priorities.
+        self.goal_queue = goals                 # convert goals into a priority queue.
+        heapq.heapify(self.goal_queue)
+
+    # The example here: https://github.com/viniciusgerevini/godot-goap/blob/main/goap/agent.gd
+    # seems to use the character class to make a plan. This makes sense--a plan is for a specific npc, after all.
+
+    # This class will define a character and the handling of the planning. Unlike actions and goals, with 1 class per action/goal,
+    # characters will be created as objects from this one class.
+
+    # This assumes priorities do not change. Eventually, we will need to implement a dynamic system that changes priorities according
+    # to external stimuli.
+
+    # get highest priority goal, without priority
+    def get_priority_goal(self) -> Goal:
+        goal = heapq.heappop(self.goal_queue)
+        return goal[1]
+    
+    # add a goal to the priority queue.
+    def add_priority_goal(self, goal: tuple[int, Goal]) -> None:
+        heapq.heappush(self.goal_queue, goal)
+
+    # get the plan for the highest priority goal.
+    def get_plan(self) -> None:
+        
+        return None
+    
--- src/goap/planner.py
+++ src/goap/planner.py
@@ -15,7 +15,12 @@ world_state: dict = {
     "hungry": True
 }
 
+
+
 # iterate through the actions to find a path until the desired state matches the world state.
+# the desired state is determined by the goal.
+# as the search, we will use an implementation of a*, though the h value will be 0, effectively making it dijkstra's algorithm.
+# speed is not the issue right now; we also need to determine a way to find a reasonable heuristic value.
 def build_plan(root):
 
     desired_state: dict = dict()
